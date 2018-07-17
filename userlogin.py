484237a95be5a3a43b07ffb4464a951a1b3d4adc
unknown <lekshmir012@gmail.com>
2018-07-17 11:54:35 +0530

    user login full

--- userlogin.py
+++ userlogin.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Resource, Api
 from flask_restful import reqparse
 import base64
+
 
 
 app = Flask(__name__)
@@ -76,8 +77,11 @@ class UserAuth(Resource):
 class UserHandling(Resource):
     def post(self):
         args = parser.parse_args()
-        users_list.append(User(**args))
-        return {"uid": user.user_id for user in users_list if user.user_name == args['user_name']}, 201
+        if not user_exists(args['user_name']):
+            users_list.append(User(**args))
+            return {"uid": user.user_id for user in users_list if user.user_name == args['user_name']}, 201
+        else:
+            return {"uid": user.user_id for user in users_list if user.user_name == args['user_name']}, 409
 
     def put(self):
         args = parser.parse_args()
@@ -89,6 +93,22 @@ class UserHandling(Resource):
                     for key, val in args.items():
                         users_list[i][key] = val
                     return {'name': users_list[i].user_name}, 200
+
+    def delete(self):
+        args = parser.parse_args()
+        if not user_exists(args['user_name']):
+            return {'message': 'user doesnt exist'}, 404
+        else:
+            for i in range(len(users_list)):
+                if users_list[i].user_name == args['user_name']:
+                    del users_list[i]
+                    return 200
+
+    def get(self):
+        datalist = {}
+        for i in range(len(users_list)):
+            datalist[users_list[i].user_id] = [users_list[i].user_name, users_list[i].role_id]
+        return datalist, 200
 
 
 api.add_resource(UserAuth, '/login')
